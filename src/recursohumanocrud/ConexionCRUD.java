51459d4a3c4673dd244beca9d45ac67dfef0e6f2
Crespin2 <101343368+Crespin2@users.noreply.github.com>
2022-04-30 22:12:02 -0600

    Conexion CRUD

---
+++ src/recursohumanocrud/ConexionCRUD.java
@@ -0,0 +1,128 @@
+//A.) DECLARAR VARIABLES CON LOS DATOS QUE SE UTILIZARAN EN NUESTRA CONEXION
+package recursohumanocrud;
+
+import java.sql.*; //Librerias para las conexiones a la BD
+
+public class ConexionCRUD {
+    /*Ruta de la base de datos del servidor 127.0.0.1, el puerto 3306 y el nombre
+    de la base de datos bd_recurso_humano
+    */
+    private final String servidor = "jdbc:mysql://127.0.0.1:3306/bd_recurso_humano";
+    //Nombre del usuario (root por defecto) de la base de datos
+    private final String usuario = "root";
+    //Clave del usuario de la base de datos
+    private final String clave="";
+    //Libreria de MySQL
+    private final String driverConector = "com.mysql.jdbc.Driver";
+    //Objeto de la clase Connection del paquete java.sql
+    private static Connection conexion;
+    
+//B.) ESTABLECER LOS METODOS PARA LA CONEXION A LA BD CON LAS VARIABLES (CONSTANTES) ANTERIORES
+    
+    public ConexionCRUD(){
+        try {
+            Class.forName(driverConector);//Levantar el driver
+            //Establecer conexion
+            conexion=DriverManager.getConnection(servidor, usuario, clave);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Conexion fallida! Error!: "+ e.getMessage());
+        }
+    }
+    
+    public Connection getConnection(){
+        return conexion; //Devuelve el objeto conexion
+    }
+    
+//C.) CREAR EL METODO PARA GUARDAR LOS CRITERIOS EN LA BD
+    
+    public void guardarRegistros(String tabla, String camposTabla, String valoresCampos){
+    //Cargar la conexion
+    ConexionCRUD conectar = new ConexionCRUD();
+    Connection cone = conectar.getConnection();
+        try {
+            //definir la sentecia SQL
+            String sqlQueryStmt = "INSERT INTO " + tabla + " (" + camposTabla + ") VALUES (" + valoresCampos + ");";
+            //Establecemos la comunicaion entre nuestra aplicion java y la base de datos
+            Statement stmt;//Envia sentencias sql a la base dde datos
+            stmt=cone.createStatement();
+            stmt.executeUpdate(sqlQueryStmt);//Ejecutar la sentencia sql
+            stmt.close();
+            cone.close();
+            System.out.println("Registro guardado correctamente!");
+        }catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+//D.) METODO PARA ACTUALIZAR Y ELIMINAR LOS REGISTROS
+    public void actualizarEliminarRegistros (String tabla, String valoresCamposNuevos, String condicion){
+    //Cargar la conexion
+    ConexionCRUD conectar = new ConexionCRUD();
+    Connection cone = conectar.getConnection();
+        try {
+            Statement stmt;
+            String sqlQueryStmt;
+            //Verificar que valoresCamposNuevos venga vacia y asi selecionar si es borrar o actualizar registro
+            if (valoresCamposNuevos.isEmpty()) {
+                sqlQueryStmt = "DELETE FROM " + tabla + " WHERE " + condicion + ";";
+            } else {
+                sqlQueryStmt = "UPDATE " + tabla + " SET " + valoresCamposNuevos + " WHERE " + condicion + ";";
+            }
+            stmt = cone.createStatement();
+            stmt.executeUpdate(sqlQueryStmt);
+            stmt.close();
+            cone.close();
+        } catch (SQLException ex) {
+            System.out.println("Ha ocurrido el siguiente error: " + ex.getMessage());
+        }
+    }
+    
+    //E.) METODO PARA HACER LAS CONSULTAS DE REGISTROS DE LA BD
+    
+    public void desplegarRegistro(String tablaBuscar, String camposBuscar, String condicionBuscar) throws SQLException{
+    //Cargar la conexion
+    ConexionCRUD conectar = new ConexionCRUD();
+    Connection cone = conectar.getConnection();
+        try {
+            Statement stmt;
+            String sqlQueryStmt;
+            if (condicionBuscar.equals("")) {
+                sqlQueryStmt = "SELECT " + camposBuscar + " FROM " + tablaBuscar + ";";
+            } else {
+                sqlQueryStmt = "SELECT " + camposBuscar + " FROM " + tablaBuscar + " WHERE " + condicionBuscar;
+            }
+            stmt = cone.createStatement();
+            stmt.executeQuery(sqlQueryStmt);
+            //Le indicamos que ejecute la consulta de la tabla y le pasamos por argumentos nuestra sentencia
+            try (ResultSet miResultSet = stmt.executeQuery(sqlQueryStmt)){
+                if (miResultSet.next()) { //Ubica el cursor en la primera fila de la tabla de resultado
+                    ResultSetMetaData metaData = miResultSet.getMetaData();
+                    int numColumnas = metaData.getColumnCount();
+                    System.out.print("<<REGISTROS ALMACENADOS>>");
+                    System.out.println();
+                    for (int i = 1; i <= numColumnas; i++) {
+                        //Muestra los titulos de las columnas y %-20\t indica la separacion entre columnas
+                        System.out.printf("%-20s\t", metaData.getColumnName(i));
+                    }
+                    System.out.println();
+                    do {                        
+                        for (int i = 1; i <=numColumnas ; i++) {
+                            System.out.printf("%-20s\t", miResultSet.getObject(i));   
+                        }
+                    } while (miResultSet.next());
+                    System.out.println();
+                } else {
+                    System.out.println("No se han encontrado registros");
+                }
+                miResultSet.close();//Cerrar el ResultSet
+            } finally{
+                //Cerrar el Statement y la Conexion; se cierran en orden inverso de como se han abierto
+                stmt.close();
+                cone.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println("Ha ocurrido el siguiente error: "+ ex.getMessage());
+        }
+    }
+    
+}
---
+++ src/recursohumanocrud/MenuPrincipal.java
@@ -0,0 +1,54 @@
+
+package recursohumanocrud;
+
+import java.sql.SQLException;//Libreria para conexiones con bases de datos
+import java.util.Scanner;
+
+public class MenuPrincipal {
+    //throws especifica el tipo de excepcion que puede ocasionar
+    public static void main(String[] args) throws SQLException {
+        desplegarMenu();
+        
+    }
+    
+    public static void desplegarMenu() throws SQLException {
+        Scanner opcionSelecionada = new Scanner(System.in);
+        String opcionMenu;
+        //Menu a desplegar
+        System.out.println("************************************");
+        System.out.println("|     CRUD DE JAVA EN CONSOLA      |");
+        System.out.println("************************************");
+        System.out.println("| Opciones:                        |");
+        System.out.println("|        1. Crear registros        |");
+        System.out.println("|        2. Consultar registros    |");
+        System.out.println("|        3. Actualizar registros   |");
+        System.out.println("|        4. Eliminar Registros     |");
+        System.out.println("|        5. Salir                  |");
+        System.out.println("***********************************");
+        System.out.print("Seleccionar opcion: ");
+        opcionMenu = opcionSelecionada.next();
+        
+        //Despliegue de menu basado en las opciones selecionadas
+        switch (opcionMenu){
+            //Las clases para el CRUD por el momento se colocan como comentarios
+            case "1":
+                //Create create = new Create();
+                break;
+            case "2":
+                //Read read = new Read();
+                break;
+            case "3":
+                //Update update = new Update();
+                break;
+            case "4":
+                //Delete delete = new Delete();
+                break;
+            case "5":
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Seleccion invalida!");
+                break;
+        }
+    }
+}
